673b3f7f322413eb9c9cf910a83ba6d6df16591e
Maksim Chendrov <maxbrofenx@gmail.com>
2018-04-30 18:31:25 +0300

    cb-2.3.3 -- add App.IoC with the ContainerBootstrap

---
+++ Callboard/Callboard.App.IoC/DependencyResolution/ContainerBootstrap.cs
@@ -0,0 +1,36 @@
+﻿using Callboard.App.Business.Abstract;
+using Callboard.App.General.Entities;
+using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Callboard.App.IoC.DependencyResolution
+{
+    public static class ContainerBootstrap
+    {
+        private static IContainer _container;
+        public static void Initialize()
+        {
+            _container = new Container(
+                x => x.Scan
+                       (
+                           scan =>
+                           {
+                               scan.AssemblyContainingType<IEntityRepository<object>>();
+                               //scan.Assembly("Callboard.App.Business");
+                               scan.WithDefaultConventions();
+                               scan.LookForRegistries();
+                           }
+                       )
+          );
+        }
+
+        public static IContainer Container
+        {
+            get { return _container; }
+        }
+    }
+}
