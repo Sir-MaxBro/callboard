eca8bea6435d31fa597f8e3b57120d83db3a418d
Maksim Chendrov <maxbrofenx@gmail.com>
2018-04-30 20:54:49 +0300

    cb-2.5.1 -- add Category repository interface and realization

---
+++ Callboard/Callboard.App.Business/Abstract/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Callboard.App.General.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Callboard.App.Business.Abstract
+{
+    public interface ICategoryRepository : IEntityRepository<Category>
+    {
+    }
+}
---
+++ Callboard/Callboard.App.Business/Concrete/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using Callboard.App.Business.Abstract;
+using Callboard.App.General.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Callboard.App.Business.Concrete
+{
+    internal class CategoryRepository : ICategoryRepository
+    {
+        private ICollection<Category> _source = new List<Category>();
+        public CategoryRepository()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Category category = new Category();
+                category.Name = $"Category {i}";
+                _source.Add(category);
+            }
+        }
+
+        public IReadOnlyCollection<Category> Items
+        {
+            get => _source.ToList();
+            set => throw new NotImplementedException();
+        }
+    }
+}
