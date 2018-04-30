48b55a692322de2c6bef130c49cb2d12dacc2528
Maksim Chendrov <maxbrofenx@gmail.com>
2018-04-30 18:17:54 +0300

    cb-2.2.2 -- add Ad repository interface adn realization

---
+++ Callboard/Callboard.App.Business/Concrete/AdRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Callboard.App.Business.Abstract;
+using Callboard.App.General.Entities;
+
+namespace Callboard.App.Business.Concrete
+{
+    internal class AdRepository : IAdRepository
+    {
+        private ICollection<Ad> _source = new List<Ad>();
+        public AdRepository()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Ad ad = new Ad();
+                ad.Name = $"Name {i}";
+                ad.Description = $"Description {i}";
+                ad.Price = i + 1000;
+                _source.Add(ad);
+            }
+        }
+
+        public IReadOnlyCollection<Ad> Items
+        {
+            get { return _source.ToList(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
