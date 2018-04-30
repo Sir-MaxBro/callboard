fc9de0e7add6895b05c0275e1c59ab5f207bb1ba
Maksim Chendrov <maxbrofenx@gmail.com>
2018-04-30 21:11:51 +0300

    cb-2.5.3 -- add the navigation bar from the categories

---
+++ Callboard/Callboard.App.Web/Models/NavViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Callboard.App.General.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Callboard.App.Web.Models
+{
+    public class NavViewModel
+    {
+        public IReadOnlyCollection<Category> Categories { get; set; }
+    }
+}
