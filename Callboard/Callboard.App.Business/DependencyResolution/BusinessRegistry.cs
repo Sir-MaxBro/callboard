c0a801a95585c46bb2a13d1fdb84b32db508b821
Maksim Chendrov <maxbrofenx@gmail.com>
2018-04-30 18:21:39 +0300

    cb-2.3.1 -- add BusinessRegistry with the dependence of IAdRepository on AdRepository

---
+++ Callboard/Callboard.App.Business/DependencyResolution/BusinessRegistry.cs
@@ -0,0 +1,25 @@
+﻿using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Callboard.App.Business.Abstract;
+using Callboard.App.Business.Concrete;
+using System.Threading.Tasks;
+using Callboard.App.General.Entities;
+
+namespace Callboard.App.Business.DependencyResolution
+{
+    public class BusinessRegistry : Registry
+    {
+        public BusinessRegistry()
+        {
+            Scan(scan =>
+            {
+                scan.TheCallingAssembly();
+                scan.WithDefaultConventions();
+            });
+            For<IAdRepository>().Use<AdRepository>();
+        }
+    }
+}
