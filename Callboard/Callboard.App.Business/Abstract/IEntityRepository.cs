1053aa6940407ba5b4d1ed0f9c1c5f73b6642670
Maksim Chendrov <maxbrofenx@gmail.com>
2018-04-30 18:16:10 +0300

    cb-2.2.1 -- create common entity repository interface

---
+++ Callboard/Callboard.App.Business/Abstract/IEntityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Callboard.App.Business.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+        IReadOnlyCollection<T> Items { get; set; }    
+    }
+}
