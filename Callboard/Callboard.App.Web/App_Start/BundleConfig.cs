db72b91a8e87e69ed474d945ae4d4a9d46cc35d5
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-01 18:31:16 +0300

    cb-2.6.2 -- add Bundles

---
+++ Callboard/Callboard.App.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Callboard.App.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .IncludeDirectory("~/Content/css", "*.css", true));
+        }
+    }
+}
