1155260ab5a0cd2344f6404e40ea1b27588210c6
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-13 09:40:28 +0300

    cb-4.1.7 -- add CommercialViewModel

--- Callboard/Callboard.App.Web/Controllers/CommercialController.cs
+++ Callboard/Callboard.App.Web/Controllers/CommercialController.cs
@@ -2,22 +2,38 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Callboard.App.Web.CommercialService;
+using Callboard.App.Web.Models;
 
 namespace Callboard.App.Web.Controllers
 {
     public class CommercialController : Controller
     {
+
         // GET: Commercial
-        public ActionResult GetCommercial()
+        public PartialViewResult GetCommercial()
         {
             ICommercialRepository commercialRepository = new CommercialRepositoryClient();
 
             var commercials = commercialRepository.GetCommercials();
+            var model = new CommercialViewModel
+            {
+                Commercials = commercials
+            };
 
-            return View();
+            return PartialView("CommercialPartial", model);
         }
+
+        //public async Task<ActionResult> RenderImage(int id)
+        //{
+        //    Item item = await db.Items.FindAsync(id);
+
+        //    byte[] photoBack = item.InternalImage;
+
+        //    return File(photoBack, "image/png");
+        //}
     }
 }
---
+++ Callboard/Callboard.App.Web/Models/CommercialViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Callboard.App.Web.Models
+{
+    public class CommercialViewModel
+    {
+        public IReadOnlyCollection<CommercialService.Commercial> Commercials { get; set; }
+    }
+}
