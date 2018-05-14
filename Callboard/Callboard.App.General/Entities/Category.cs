bc9934a2e3a0513c288e0bbad3ad5fbda5543fc5
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-14 06:42:26 +0300

    cb-4.3.14 -- add attributes for Category

--- Callboard/Callboard.App.General/Entities/Category.cs
+++ Callboard/Callboard.App.General/Entities/Category.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
+using Callboard.App.General.Attributes;
 
 namespace Callboard.App.General.Entities
 {
+    [Table("Category")]
     public class Category
     {
+        [Column("CategoryId")]
         public int Id { get; set; }
 
+        [Column("Name")]
         public string Name { get; set; }
 
         public ICollection<Subcategory> Subcategories { get; set; }
