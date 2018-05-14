a5a10a27ed4af3bef4aad88756b3993f381ddaf9
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-14 08:05:55 +0300

    cb-5.1.2 -- add sqldbcontext

---
+++ Callboard/Callboard.App.Data/Abstract/SqlDbContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Callboard.App.General.Attributes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Callboard.App.Data.Abstract
+{
+    public abstract class SqlDbContext<T>
+    {
+        private DbConnection _dbConnection = new SqlConnection();
+        public SqlDbContext(string connectionString)
+        {
+            _dbConnection.ConnectionString = connectionString;
+        }
+
+        public void Open()
+        {
+            try
+            {
+                _dbConnection.Open();
+            }
+            catch (SqlException ex)
+            {
+
+            }
+        }
+
+        public void Close()
+        {
+            _dbConnection?.Close();
+        }
+
+        public IReadOnlyCollection<T> Select()
+        {
+            var result = new List<T>();
+            var tableNames = typeof(T).GetCustomAttributes(false)
+                .OfType<TableAttribute>()
+                .Select(attr => attr.TableName)
+                .ToList();
+
+            string spName = $"sp_select_{ tableNames[0] }";
+            DbCommand command = new SqlCommand(spName);
+            command.Connection = _dbConnection;
+            command.CommandType = CommandType.StoredProcedure;
+
+            var reader = command.ExecuteReader();
+
+
+
+            return result;
+        }
+    }
+}
