e4e2a06700a2d5ef52096a30ae3cf6153523fcc4
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-14 06:42:02 +0300

    cb-4.3.13 -- add attributes for City

--- Callboard/Callboard.App.General/Entities/City.cs
+++ Callboard/Callboard.App.General/Entities/City.cs
@@ -1,17 +1,22 @@
-﻿namespace Callboard.App.General.Entities
+﻿using Callboard.App.General.Attributes;
+
+namespace Callboard.App.General.Entities
 {
+    [Table("City")]
     public class City
     {
+        [Column("CityId")]
         public int Id { get; set; }
 
+        [Column("AreaId")]
         public int AreaId { get; set; }
 
         //public Area Area { get; set; }
-
+        [Column("CountryId")]
         public int CountryId { get; set; }
 
         //public Country Country { get; set; }
-
+        [Column("Name")]
         public string Name { get; set; }
     }
 }
