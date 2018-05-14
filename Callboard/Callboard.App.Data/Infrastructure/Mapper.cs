fcd469926075fb5b356d2e7dfed138f5f920f1d5
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-14 08:06:41 +0300

    cb-5.1.3 -- add mapper

---
+++ Callboard/Callboard.App.Data/Infrastructure/Mapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Callboard.App.Data.Infrastructure
+{
+    internal class Mapper
+    {
+        public static T Map<T>()
+            where T : new()
+        {
+            return new T();
+        }
+    }
+}
