8685fe91ad7fa41909b3047a1df75f32545e053a
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-14 06:40:49 +0300

    cb-4.3.10 -- add attributes for Location

--- Callboard/Callboard.App.General/Entities/Location.cs
+++ Callboard/Callboard.App.General/Entities/Location.cs
@@ -1,21 +1,26 @@
-﻿namespace Callboard.App.General.Entities
+﻿using Callboard.App.General.Attributes;
+
+namespace Callboard.App.General.Entities
 {
+    [Table("Location")]
     public class Location
     {
+        [Column("LocationId")]
         public int Id { get; set; }
 
         //public int CityId { get; set; }
-
+        [ForeignKey("CityId")]
         public City City { get; set; }
 
         //public int AreaId { get; set; }
-
+        [ForeignKey("AreaId")]
         public Area Area { get; set; }
 
         //public int CountryId { get; set; }
-
+        [ForeignKey("CountryId")]
         public Country Country { get; set; }
 
+        [Column("AddressLine")]
         public string AddressLine { get; set; }
     }
 }
