9b5a71ee41e7a6860e37a2d1dd37f5b5bca02fb3
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-15 17:54:03 +0300

    cb-5.1.14 -- add mapper functionality

--- Callboard/Callboard.App.Data/Infrastructure/Mapper.cs
+++ Callboard/Callboard.App.Data/Infrastructure/Mapper.cs
@@ -18,16 +18,27 @@ namespace Callboard.App.Data.Infrastructure
 
             while (reader.Read())
             {
-                var dic = Enumerable.Range(0, reader.FieldCount).ToDictionary(reader.GetName, reader.GetValue);
-                item = Map<T>(dic);
-                //item = Map<T>(reader);           
+                item = Map<T>(reader);
                 mappingCollection.Add(item);
             }
 
             return mappingCollection;
         }
 
-        private static T Map<T>(/*DbDataReader reader*/IDictionary<string, object> dicReader)
+        public static T MapElement<T>(DbDataReader reader)
+            where T : new()
+        {
+            T item = new T();
+
+            while (reader.Read())
+            {
+                item = Map<T>(reader);
+            }
+
+            return item;
+        }
+
+        private static T Map<T>(DbDataReader reader)
             where T : new()
         {
             T item = new T();
@@ -35,29 +46,44 @@ namespace Callboard.App.Data.Infrastructure
 
             for (int i = 0; i < properties.Length; i++)
             {
-                var columnType = properties[i].PropertyType;
-                var columnName = properties[i].GetCustomAttributes(typeof(ColumnAttribute), false)
+                var property = properties[i];
+                var columnType = property.PropertyType;
+                // get column name from attribute
+                var columnName = property.GetCustomAttributes(typeof(ColumnAttribute), false)
                     .OfType<ColumnAttribute>()
-                    .ElementAtOrDefault(0)?
-                    .ColumnName;
-                if (columnName == null)
-                {
-                    columnName = properties[i].Name;
-                }
+                    .ElementAtOrDefault(0)
+                    ?.ColumnName;
 
-                try
+                if (columnName == null && columnType.IsClass)
                 {
-                    var columnValue = dicReader[columnName];
-                    //var columnValue = reader[columnName];
-                    if (columnValue != null)
+                    // create new instance of dependence type
+                    var innerObj = columnType.GetConstructor(new Type[] { }).Invoke(new object[] { });
+                    // get foreign key from attribute
+                    var fKeyName = property.GetCustomAttributes(typeof(ForeignKeyAttribute), false)
+                        .OfType<ForeignKeyAttribute>()
+                        .ElementAtOrDefault(0)
+                        ?.FKey;
+                    var fKeyValue = reader[fKeyName];
+                    var propertyInner = innerObj.GetType().GetProperties()
+                        .Where(prop => prop.GetCustomAttributes(typeof(ColumnAttribute), false).OfType<ColumnAttribute>().FirstOrDefault().ColumnName == fKeyName)
+                        .FirstOrDefault();
+
+                    propertyInner?.SetValue(innerObj, fKeyValue);
+
+                    property.SetValue(item, innerObj);
+                }
+                else if (!columnType.IsInterface) // crutch
+                {
+                    try
                     {
-                        columnValue = Convert.ChangeType(columnValue, columnType);
-                        properties[i].SetValue(item, columnValue);
+                        var columnValue = reader[columnName];
+                        if (columnValue != null)
+                        {
+                            property.SetValue(item, columnValue);
+                        }
                     }
-                }
-                catch (KeyNotFoundException ex)
-                {
-
+                    catch (IndexOutOfRangeException ex) { }
+                    catch(ArgumentException ex) { }
                 }
             }
             return item;
