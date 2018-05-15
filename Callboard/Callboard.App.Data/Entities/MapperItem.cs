1f39676c9de6fb561f7d6d6d21561b7da3fa135a
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-15 17:55:20 +0300

    cb-5.1.15 -- add mapperItem

---
+++ Callboard/Callboard.App.Data/Entities/MapperItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace Callboard.App.Data.Entities
+{
+    internal class MapperItem
+    {
+        public string TableName { get; set; }
+        public string FKey { get; set; }
+        public object Value { get; set; }
+        public Type ValueType { get; set; }
+        public PropertyInfo Property { get; set; }
+    }
+}
