dd7411a9ed02c60d0762e0ca1c608c6738e01edc
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-17 16:14:45 +0300

    hf-1.1.12 -- add identity in State table

--- database/tables/State.sql
+++ database/tables/State.sql
@@ -1,7 +1,7 @@
 USE [callboardDB];
 CREATE TABLE [dbo].[State]
 (
-    [StateId]   INT NOT NULL,
+    [StateId]   INT IDENTITY(1, 1) NOT NULL,
     [Condition] NVARCHAR(50) NOT NULL,
 
     PRIMARY KEY CLUSTERED ([StateId] ASC)
