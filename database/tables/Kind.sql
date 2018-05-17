db9824626298a1704f39c38bad142298cd3b1e62
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-17 16:14:32 +0300

    hf-1.1.11 -- add identity in Kind table

--- database/tables/Kind.sql
+++ database/tables/Kind.sql
@@ -1,7 +1,7 @@
 USE [callboardDB];
 CREATE TABLE [dbo].[Kind]
 (
-    [KindId] INT NOT NULL,
+    [KindId] INT IDENTITY(1, 1) NOT NULL,
     [Type]   NVARCHAR(50) NOT NULL,
 
     PRIMARY KEY CLUSTERED ([KindId] ASC)
