2ccc5c982b06c183c51feb5dccb01ecef6bec44d
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-18 14:35:56 +0300

    hf-1.3.2 -- add InvalidIdException

---
+++ Callboard/Callboard.App.General/Exceptions/InvalidIdException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Callboard.App.General.Exceptions
+{
+    public class InvalidIdException : ApplicationException
+    {
+        public InvalidIdException() { }
+
+        public InvalidIdException(string message)
+            : base(message) { }
+
+        public InvalidIdException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        protected InvalidIdException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+    }
+}
