d0b9ab15b15f81b0d212b4e91669030ec27b7cca
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-18 14:27:16 +0300

    hf-1.3.1 -- add general checker __ checker has a CheckForNull method

---
+++ Callboard/Callboard.App.General/Helpers/Checker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Callboard.App.General.Helpers
+{
+    public static class Checker
+    {
+        public static void CheckForNull<T>(T obj)
+            where T : class
+        {
+            if (Equals(obj, null))
+            {
+                throw new NullReferenceException($"Object { typeof(T) } is null");
+            }
+        }
+    }
+}
