7029a4bfc6c2f9b9d5628c018a6e045ae13b3bcb
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-19 11:25:41 +0300

    hf-1.7.12 -- add data config section

---
+++ Callboard/Callboard.App.Data/Config/Sections/DataSettingsConfigSection.cs
@@ -0,0 +1,14 @@
+﻿using Callboard.App.Data.Config.Collections;
+using System.Configuration;
+
+namespace Callboard.App.Data.Config.Sections
+{
+    public class DataSettingsConfigSection : ConfigurationSection
+    {
+        [ConfigurationProperty("tables")]
+        public TableCollection Tables
+        {
+            get { return ((TableCollection)(base["tables"])); }
+        }
+    }
+}
