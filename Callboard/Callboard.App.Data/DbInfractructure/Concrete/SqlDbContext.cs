bceeca26caf79a5ee9ac5c1462a1f4be13614371
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-19 18:31:05 +0300

    hf-1.8.5 -- add realization dbContext interface for Sql

---
+++ Callboard/Callboard.App.Data/DbInfractructure/Concrete/SqlDbContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Callboard.App.Data.DbInfractructure
+{
+    internal class SqlDbContext : IDbContext
+    {
+        private SqlConnection _connection;
+        public SqlDbContext(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+        }
+
+        public void Open()
+        {
+            try
+            {
+                _connection.Open();
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw ex;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Close()
+        {
+            this.Dispose();
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+        }
+
+        public DbDataReader ExecuteProcedure(string procedureName, IDictionary<string, object> values = null)
+        {
+            SqlCommand command = new SqlCommand(procedureName, _connection)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+            };
+
+            if (values != null)
+            {
+                foreach (var item in values)
+                {
+                    command.Parameters.AddWithValue($"@{ item.Key }", item.Value);
+                }
+            }
+
+            DbDataReader reader = null;
+            try
+            {
+                reader = command.ExecuteReader();
+            }
+            catch (InvalidCastException ex)
+            {
+                throw ex;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw ex;
+            }
+            return reader;
+        }
+    }
+}
