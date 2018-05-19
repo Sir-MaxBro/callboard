4b1f07c7f07c354b029af8872e2a13f084c31cb3
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-19 11:20:54 +0300

    hf-1.7.4 -- add AdRepository stub in App.Data

--- Callboard/Callboard.App.Data/Abstract/IAdRepository.cs
+++ Callboard/Callboard.App.Data/Abstract/IAdRepository.cs
@@ -1,9 +1,5 @@
 ﻿using Callboard.App.General.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Callboard.App.Data.Abstract
 {
