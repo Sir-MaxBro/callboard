368c256bad47b6e3eef4b70f806bc9f18803205f
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-19 09:36:22 +0300

    hf-1.7.2 -- add IAdRepository in App.Data

---
+++ Callboard/Callboard.App.Data/Abstract/IAdRepository.cs
@@ -0,0 +1,16 @@
+﻿using Callboard.App.General.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Callboard.App.Data.Abstract
+{
+    public interface IAdRepository : IEntityRepository<Ad>
+    {
+        Ad GetAd(int id);
+
+        IReadOnlyCollection<Ad> GetAdsByCategoryId(int categoryId);
+    }
+}
