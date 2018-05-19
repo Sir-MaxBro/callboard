3caf0da77dc0f0cb8fb595f89dc27908ad932394
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-19 12:40:20 +0300

    hf-1.1.36 -- return adId from sp_select_image_by_adid script

--- database/sp/_select/sp_select_image_by_adid.sql
+++ database/sp/_select/sp_select_image_by_adid.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE [dbo].[sp_select_image_by_adid]
     @AdId INT
 AS
 (
-    SELECT [Image].[ImageId], [Data], [MimeType]
+    SELECT [Image].[ImageId] AS [ImageId], [Data], [MimeType], [ImagesInAds].[AdId] AS [AdId]
     FROM [dbo].[Image]
     INNER JOIN [dbo].[ImagesInAds]
     ON [Image].[ImageId] = [ImagesInAds].[ImageId]
