4984a635432607e7d46586a9d5a85145f9738904
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-19 16:17:50 +0300

    hf-1.7.21 -- add ConfigurationHelper __ for get configuration from assembly

---
+++ Callboard/Callboard.App.General/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.Reflection;
+
+namespace Callboard.App.General.Helpers
+{
+    public static class ConfigurationHelper
+    {
+        public static Configuration GetAssemblyConfiguration<T>(T obj)
+        {
+            var assembly = GetObjAssembly(obj);
+            return ConfigurationManager.OpenExeConfiguration(assembly.Location);
+        }
+
+        public static Configuration GetAssemblyConfiguration(Assembly assembly)
+        {
+            return ConfigurationManager.OpenExeConfiguration(assembly.Location);
+        }
+
+        public static Assembly GetObjAssembly<T>(T obj)
+        {
+            return obj.GetType().Assembly;
+        }
+    }
+}
