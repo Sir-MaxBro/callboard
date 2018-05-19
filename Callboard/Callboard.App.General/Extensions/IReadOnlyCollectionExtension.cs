3397ad3e172e271ab0bd0a18d6680ef8e4fd20d2
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-19 20:28:01 +0300

    hf-1.9 -- add IReadOnlyCollection extension __ FirstOrDefault

---
+++ Callboard/Callboard.App.General/Extensions/IReadOnlyCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Callboard.App.General.Extensions
+{
+    public static class IReadOnlyCollectionExtension
+    {
+        public static T FirstOfDefault<T>(this IReadOnlyCollection<T> source, Predicate<T> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+            }
+            return default(T);
+        }
+    }
+}
