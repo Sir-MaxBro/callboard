0d6be2d6cdc6c3af1b04c9944c936b2f1fc36a63
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-19 18:30:12 +0300

    hf-1.8.3 -- add dbConnection interface

---
+++ Callboard/Callboard.App.Data/DbInfractructure/Abstract/IDbConnection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Callboard.App.Data.DbInfractructure
+{
+    internal interface IDbConnection : IDisposable
+    {
+        void Open();
+
+        void Close();
+    }
+}
