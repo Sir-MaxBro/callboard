89ec2d207c108f01e03ed392808e38f4073d7ae3
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-19 12:41:08 +0300

    hf-1.1.38 -- return userId from sp_select_phone_by_userid script

--- database/sp/_select/sp_select_phone_by_userid.sql
+++ database/sp/_select/sp_select_phone_by_userid.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE [dbo].[sp_select_phone_by_userid]
     @UserId INT
 AS
 (
-    SELECT [PhoneId], [Number]
+    SELECT [PhoneId], [Number], [UserId]
     FROM [dbo].[Phone]
     WHERE [Phone].[UserId] = @UserId
 );
