90c6e4ecc0d9184f0dcf80f3a2448fb203616b82
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-19 18:31:42 +0300

    hf-1.8.6 -- add DataContext for join dbContext and Mapper

---
+++ Callboard/Callboard.App.Data/Infrastructure/DataContext.cs
@@ -0,0 +1,55 @@
+﻿using Callboard.App.Data.DbInfractructure;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace Callboard.App.Data.Infrastructure
+{
+    internal class DataContext : IDisposable
+    {
+        private IDbContext _context;
+        public DataContext(string connectionString)
+        {
+            _context = new SqlDbContext(connectionString);
+        }
+
+        public void Open()
+        {
+            _context.Open();
+        }
+
+        public void Close()
+        {
+            this.Dispose();
+        }
+
+        public IReadOnlyCollection<TResult> ExecuteProcedure<TResult>(string procedureName, IDictionary<string, object> values, Func<DbDataReader, TResult> mapping)
+        {
+            var reader = _context.ExecuteProcedure(procedureName, values);
+
+            IReadOnlyCollection<TResult> mappingCollection = null;
+            if (!reader.IsClosed)
+            {
+                mappingCollection = Mapper.MapCollection(reader, mapping);
+                reader.Close();
+            }
+            return mappingCollection;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        private IReadOnlyCollection<TResult> MapCollection<TResult>(DbDataReader reader, Func<DbDataReader, TResult> mapping)
+        {
+            IReadOnlyCollection<TResult> mappingCollection = null;
+            if (!reader.IsClosed)
+            {
+                mappingCollection = Mapper.MapCollection(reader, mapping);
+                reader.Close();
+            }
+            return mappingCollection;
+        }
+    }
+}
