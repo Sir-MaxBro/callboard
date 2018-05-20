37531809c8366fe73d7a0ab8f3545f805b131a7d
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-20 15:39:54 +0300

    hf-1.9.10 -- add abstract EntityRepository in App.Data __ with common repository logic

---
+++ Callboard/Callboard.App.Data/Repositories/Abstract/EntityRepository.cs
@@ -0,0 +1,33 @@
+﻿using Callboard.App.Data.Entities;
+using Callboard.App.Data.Infrastructure;
+using Callboard.App.General.Extensions;
+
+namespace Callboard.App.Data.Repositories
+{
+    internal abstract class EntityRepository
+    {
+        protected string _connectionString;
+        protected Table _table;
+        public EntityRepository()
+        {
+            var configuration = new DataConfiguration();
+            _connectionString = configuration.ConnectionString;
+            _table = configuration.GetTable(this.TableName);
+        }
+
+        protected abstract string TableName { get; }
+
+        protected Procedure GetProcedure(string procedureType)
+        {
+            var procedure = _table.Procedures.FirstOfDefault(item => item.Type == procedureType);
+            return procedure;
+        }
+
+        protected string GetName(string propertyName)
+        {
+            var columns = _table.Columns;
+            string name = columns.FirstOfDefault(item => item.MapPropertyName.ToLower() == propertyName)?.Name;
+            return name;
+        }
+    }
+}
