3277f5f4ea22767aeab7c8f0f575281d1ff8c741
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-20 16:54:24 +0300

    hf-1.10.2 -- add App.Data CommercialRepository interface

---
+++ Callboard/Callboard.App.Data/Repositories/Abstract/ICommercialRepository.cs
@@ -0,0 +1,8 @@
+﻿using Callboard.App.General.Entities.Commercial;
+
+namespace Callboard.App.Data.Repositories
+{
+    public interface ICommercialRepository : IEntityRepository<Commercial>
+    {
+    }
+}
