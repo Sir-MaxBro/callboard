dc802726f73f509fcf346f4913ef24381a2a507e
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-20 15:43:41 +0300

    hf-1.9.15 -- add App.Data dependency container

---
+++ Callboard/Callboard.App.Data/DependencyResolution/DataRegistry.cs
@@ -0,0 +1,15 @@
+﻿using Callboard.App.Data.Repositories;
+using StructureMap;
+
+namespace Callboard.App.Data.DependencyResolution
+{
+    public class DataRegistry : Registry
+    {
+        public DataRegistry()
+        {
+            For<IAdRepository>().Use<AdRepository>();
+            For<ICategoryRepository>().Use<CategoryRepository>();
+            For<IAdDetailsRepository>().Use<AdDetailsRepository>();
+        }
+    }
+}
