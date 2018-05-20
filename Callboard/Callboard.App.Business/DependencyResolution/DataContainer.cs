c63b36884bde71f2ecebb030d20ecfd637cae6be
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-20 15:47:10 +0300

    hf-1.9.18 -- add in App.Business DataContainer

---
+++ Callboard/Callboard.App.Business/DependencyResolution/DataContainer.cs
@@ -0,0 +1,38 @@
+﻿using StructureMap;
+using Data = Callboard.App.Data.Repositories;
+
+namespace Callboard.App.Business.DependencyResolution
+{
+    internal static class DataContainer
+    {
+        private static IContainer _container;
+        static DataContainer()
+        {
+            Initialize();
+        }
+
+        private static void Initialize()
+        {
+            _container = new Container(
+                x => x.Scan
+                       (
+                           scan =>
+                           {
+                               scan.AssemblyContainingType<Data::IEntityRepository<object>>();
+                               scan.LookForRegistries();
+                           }
+                       )
+            );
+        }
+
+        public static IContainer Container
+        {
+            get { return _container; }
+        }
+
+        public static T GetInstance<T>()
+        {
+            return _container.GetInstance<T>();
+        }
+    }
+}
