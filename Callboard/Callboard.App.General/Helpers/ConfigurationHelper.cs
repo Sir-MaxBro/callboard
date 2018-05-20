e19aca24d4c8f4625f53f5d1a38d6ad152ff602f
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-20 15:46:08 +0300

    hf-1.9.17 -- add in ConfigurationHelper method GetExecutingAssemblyConfig

--- Callboard/Callboard.App.General/Helpers/ConfigurationHelper.cs
+++ Callboard/Callboard.App.General/Helpers/ConfigurationHelper.cs
@@ -1,19 +1,15 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Reflection;
 
 namespace Callboard.App.General.Helpers
 {
     public static class ConfigurationHelper
     {
-        public static Configuration GetAssemblyConfiguration<T>(T obj)
+        public static Configuration GetExecutingAssemblyConfig<T>(T obj)
         {
-            var assembly = GetObjAssembly(obj);
-            return ConfigurationManager.OpenExeConfiguration(assembly.Location);
-        }
-
-        public static Configuration GetAssemblyConfiguration(Assembly assembly)
-        {
-            return ConfigurationManager.OpenExeConfiguration(assembly.Location);
+            var dllPath = new Uri(obj.GetType().Assembly.GetName().CodeBase).LocalPath;
+            return ConfigurationManager.OpenExeConfiguration(dllPath);
         }
 
         public static Assembly GetObjAssembly<T>(T obj)
