dcfcc0c5e46a6304bb763eb5cff1a05f934ee718
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-20 16:54:49 +0300

    hf-1.10.3 -- add App.Data CommercialRepository realization

---
+++ Callboard/Callboard.App.Data/Repositories/Realizations/CommercialRepository.cs
@@ -0,0 +1,53 @@
+﻿using Callboard.App.General.Entities.Commercial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using Service = Callboard.App.Data.CommercialService;
+
+namespace Callboard.App.Data.Repositories
+{
+    internal class CommercialRepository : ICommercialRepository
+    {
+        public IReadOnlyCollection<Commercial> Items => GetCommercials();
+
+        private IReadOnlyCollection<Commercial> GetCommercials()
+        {
+            Service::ICommercialContract contract = new Service::CommercialContractClient();
+            List<Commercial> commercials = null;
+            try
+            {
+                Func<Service::Commercial, Commercial> mapper = commercial =>
+                {
+                    var resCommercial = new Commercial
+                    {
+                        Image = new Image
+                        {
+                            Data = commercial.Image.Data,
+                            Extension = commercial.Image.Extension
+                        }
+                    };
+                    return resCommercial;
+                };
+                commercials = contract.GetCommercials().Select(mapper).ToList();
+            }
+            catch(TimeoutException ex)
+            {
+
+            }
+            catch(FaultException ex)
+            {
+
+            }
+            catch (CommunicationException ex)
+            {
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return commercials;
+        }
+    }
+}
