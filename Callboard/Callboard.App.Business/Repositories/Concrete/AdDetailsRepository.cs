52d2f0b0374333fd0403f5ddacef0094763c6d72
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-20 15:49:59 +0300

    hf-1.9.20 -- realize repositories logic from App.Data

---
+++ Callboard/Callboard.App.Business/Repositories/Concrete/AdDetailsRepository.cs
@@ -0,0 +1,23 @@
+﻿using Callboard.App.Business.DependencyResolution;
+using Callboard.App.General.Entities;
+using System.Collections.Generic;
+using Data = Callboard.App.Data.Repositories;
+
+namespace Callboard.App.Business.Repositories
+{
+    internal class AdDetailsRepository : IAdDetailsRepository
+    {
+        private Data::IAdDetailsRepository _repository;
+        public AdDetailsRepository()
+        {
+            _repository = DataContainer.GetInstance<Data::IAdDetailsRepository>();
+        }
+
+        public IReadOnlyCollection<AdDetails> Items => _repository.Items;
+
+        public AdDetails GetAdDetails(int adId)
+        {
+            return _repository.GetAdDetails(adId);
+        }
+    }
+}
---
+++ Callboard/Callboard.App.Business/Repositories/Concrete/AdRepository.cs
@@ -0,0 +1,26 @@
+﻿using Callboard.App.Business.DependencyResolution;
+using Callboard.App.General.Entities;
+using System.Collections.Generic;
+using Data = Callboard.App.Data.Repositories;
+
+namespace Callboard.App.Business.Repositories
+{
+    internal class AdRepository : IAdRepository
+    {
+        private Data::IAdRepository _repository;
+        public AdRepository()
+        {
+            _repository = DataContainer.GetInstance<Data::IAdRepository>();
+        }
+
+        public IReadOnlyCollection<Ad> Items
+        {
+            get => _repository.Items;
+        }
+
+        public IReadOnlyCollection<Ad> GetAdsByCategoryId(int categoryId)
+        {
+            return _repository.GetAdsByCategoryId(categoryId);
+        }
+    }
+}
