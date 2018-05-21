d768c470bae3c8b17f35eb5e9fd18d7f8dc5eeba
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-21 17:45:23 +0300

    hf-1.12.1 -- moved connection logic in EntityRepository

--- Callboard/Callboard.App.Data/Repositories/Abstract/EntityRepository.cs
+++ Callboard/Callboard.App.Data/Repositories/Abstract/EntityRepository.cs
@@ -2,6 +2,9 @@
 using Callboard.App.Data.Infrastructure;
 using Callboard.App.General.Extensions;
 using Callboard.App.General.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
 
 namespace Callboard.App.Data.Repositories
 {
@@ -29,6 +32,50 @@ namespace Callboard.App.Data.Repositories
         {
             string name = _table.Columns.FirstOfDefault(item => item.MapPropertyName.ToLower() == propertyName)?.Name;
             return name;
+        }
+
+        public IReadOnlyCollection<T> GetEntities<T>(string procedureName, Func<DbDataReader, T> mapper, params object[] values)
+        {
+            IReadOnlyCollection<T> entities = null;
+            using (var context = new DataContext(_connectionString))
+            {
+                var procedure = this.GetProcedure(procedureName);
+                if (procedure != null)
+                {
+                    IDictionary<string, object> inputValues = null;
+                    if (values.Length != 0)
+                    {
+                        inputValues = new Dictionary<string, object>
+                        {
+                            { procedure.Params[0], values[0] }
+                        };
+                    }
+                    entities = context.ExecuteProcedure(procedure.ProcedureName, inputValues, mapper);
+                }
+            }
+            return entities;
+        }
+
+        public T GetEntity<T>(string procedureName, Func<DbDataReader, T> mapper, params object[] values)
+        {
+            T entity = default(T);
+            using (var context = new DataContext(_connectionString))
+            {
+                var procedure = this.GetProcedure(procedureName);
+                if (procedure != null)
+                {
+                    IDictionary<string, object> inputValues = null;
+                    if (values.Length != 0)
+                    {
+                        inputValues = new Dictionary<string, object>
+                        {
+                            { procedure.Params[0], values[0] }
+                        };
+                    }
+                    entity = context.ExecuteProcedure(procedure.ProcedureName, inputValues, mapper).FirstOfDefault();
+                }
+            }
+            return entity;
         }
     }
 }
