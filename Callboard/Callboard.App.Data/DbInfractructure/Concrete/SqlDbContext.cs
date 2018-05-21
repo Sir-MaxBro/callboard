151aa3074d901f407e58b6dda7a8baa6edf583b1
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-21 14:29:42 +0300

    hf-1.11.6 -- add logging in SqlDbContext

--- Callboard/Callboard.App.Data/DbInfractructure/Concrete/SqlDbContext.cs
+++ Callboard/Callboard.App.Data/DbInfractructure/Concrete/SqlDbContext.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Data;
+using Callboard.App.General.Helpers;
+using Callboard.App.General.Loggers;
 
 namespace Callboard.App.Data.DbInfractructure
 {
     internal class SqlDbContext : IDbContext
     {
+        private ILoggerWrapper _logger = LoggerWrapper.GetInstance();
         private SqlConnection _connection;
         public SqlDbContext(string connectionString)
         {
+            Checker.CheckForNull(connectionString, $"Connection string is empty.");
             _connection = new SqlConnection(connectionString);
         }
 
@@ -25,7 +29,8 @@ namespace Callboard.App.Data.DbInfractructure
             }
             catch (InvalidOperationException ex)
             {
-                throw ex;
+                string errorMessage = $"Cannot open a connection without specifying a data source or server.\nConnection string: { _connection.ConnectionString }";
+                throw new InvalidOperationException(errorMessage, ex);
             }
             catch (SqlException ex)
             {
@@ -45,7 +50,7 @@ namespace Callboard.App.Data.DbInfractructure
 
         public DbDataReader ExecuteProcedure(string procedureName, IDictionary<string, object> values = null)
         {
-            SqlCommand command = new SqlCommand(procedureName, _connection)
+            SqlCommand command = new SqlCommand("", _connection)
             {
                 CommandType = CommandType.StoredProcedure
             };
