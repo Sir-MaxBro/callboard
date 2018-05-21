fd6d622f1fe3b7840dab85b68b9f3a1cf0817c32
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-21 14:29:24 +0300

    hf-1.11.5 -- add logging in DataContext

--- Callboard/Callboard.App.Data/Infrastructure/DataContext.cs
+++ Callboard/Callboard.App.Data/Infrastructure/DataContext.cs
@@ -1,13 +1,16 @@
 ﻿using Callboard.App.Data.DbInfractructure;
+using Callboard.App.General.Loggers;
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace Callboard.App.Data.Infrastructure
 {
     internal class DataContext : IDisposable
     {
         private IDbContext _context;
+        private ILoggerWrapper _logger = LoggerWrapper.GetInstance();
         public DataContext(string connectionString)
         {
             _context = new SqlDbContext(connectionString);
@@ -25,19 +28,34 @@ namespace Callboard.App.Data.Infrastructure
 
         public IReadOnlyCollection<TResult> ExecuteProcedure<TResult>(string procedureName, IDictionary<string, object> values, Func<DbDataReader, TResult> mapping)
         {
-            this.Open();
-
-            var reader = _context.ExecuteProcedure(procedureName, values);
-
             IReadOnlyCollection<TResult> mappingCollection = null;
-            if (!reader.IsClosed)
+            DbDataReader reader = null;
+            try
             {
-                mappingCollection = Mapper.MapCollection(reader, mapping);
-                reader.Close();
+                this.Open();
+                reader = _context.ExecuteProcedure(procedureName, values);
+                if (!reader.IsClosed)
+                {
+                    mappingCollection = Mapper.MapCollection(reader, mapping);
+                }
+                this.Dispose();
             }
-
-            this.Dispose();
-
+            catch (InvalidCastException ex)
+            {
+                _logger.ErrorFormat(ex.Message);
+            }
+            catch (SqlException ex)
+            {
+                _logger.ErrorFormat(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.ErrorFormat(ex.Message);
+            }
+            finally
+            {
+                reader?.Close();
+            }
             return mappingCollection;
         }
 
