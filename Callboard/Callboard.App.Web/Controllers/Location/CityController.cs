263ea1609a8207360d793a976f9cafdd79131cdb
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-22 17:37:16 +0300

    hf-1.18.2 -- add CityController

---
+++ Callboard/Callboard.App.Web/Controllers/Location/CityController.cs
@@ -0,0 +1,23 @@
+﻿using Callboard.App.Business.Repositories;
+using Callboard.App.General.Helpers;
+using System.Web.Mvc;
+
+namespace Callboard.App.Web.Controllers
+{
+    public class CityController : Controller
+    {
+        private ICityRepository _repository;
+        public CityController(ICityRepository repository)
+        {
+            Checker.CheckForNull(repository, $"Cannot find { typeof(ICityRepository) } in App.Web CityController");
+            _repository = repository;
+        }
+
+        public PartialViewResult GetCity(int cityId)
+        {
+            Checker.CheckId(cityId, $"CityId in GetCity method is not valid.");
+            var model = _repository.GetCity(cityId);
+            return PartialView(model);
+        }
+    }
+}
