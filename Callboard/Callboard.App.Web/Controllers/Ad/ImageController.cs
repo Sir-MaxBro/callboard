4fde187697820bd4fff433accd71eff2d64c8214
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-22 09:44:50 +0300

    hf-1.13.11 -- add ImageControlles

---
+++ Callboard/Callboard.App.Web/Controllers/Ad/ImageController.cs
@@ -0,0 +1,22 @@
+﻿using Callboard.App.Business.Repositories;
+using Callboard.App.General.Helpers;
+using System.Web.Mvc;
+
+namespace Callboard.App.Web.Controllers
+{
+    public class ImageController : Controller
+    {
+        private IImageRepository _repository;
+        public ImageController(IImageRepository repository)
+        {
+            Checker.CheckForNull(repository, $"Cannot find { typeof(IImageRepository) } in App.Web ImageController");
+            _repository = repository;
+        }
+
+        public PartialViewResult GetImagesByAdId(int adId)
+        {
+            var model = _repository.GetImagesByAdId(adId);
+            return PartialView("ImagesSlider", model);
+        }
+    }
+}
