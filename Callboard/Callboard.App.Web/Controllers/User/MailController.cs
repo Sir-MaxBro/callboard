ddfaa1f3bdc30c648406b29f180b0a7ae487e53d
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-22 17:32:29 +0300

    hf-1.18.1 -- add Mail and Phone controllers

---
+++ Callboard/Callboard.App.Web/Controllers/User/MailController.cs
@@ -0,0 +1,23 @@
+﻿using Callboard.App.Business.Repositories;
+using Callboard.App.General.Helpers;
+using System.Web.Mvc;
+
+namespace Callboard.App.Web.Controllers
+{
+    public class MailController : Controller
+    {
+        private IMailRepository _repository;
+        public MailController(IMailRepository repository)
+        {
+            Checker.CheckForNull(repository, $"Cannot find { typeof(IMailRepository) } in App.Web MailController");
+            _repository = repository;
+        }
+
+        public PartialViewResult GetMailsByUserId(int userId)
+        {
+            Checker.CheckId(userId, $"UserId in GetMailsByUserId method is not valid.");
+            var model = _repository.GetMailsByUserId(userId);
+            return PartialView(model);
+        }
+    }
+}
---
+++ Callboard/Callboard.App.Web/Controllers/User/PhoneController.cs
@@ -0,0 +1,23 @@
+﻿using Callboard.App.Business.Repositories;
+using Callboard.App.General.Helpers;
+using System.Web.Mvc;
+
+namespace Callboard.App.Web.Controllers
+{
+    public class PhoneController : Controller
+    {
+        private IPhoneRepository _repository;
+        public PhoneController(IPhoneRepository repository)
+        {
+            Checker.CheckForNull(repository, $"Cannot find { typeof(IPhoneRepository) } in App.Web PhoneController");
+            _repository = repository;
+        }
+
+        public PartialViewResult GetPhonesByUserId(int userId)
+        {
+            Checker.CheckId(userId, $"UserId in GetPhonesByUserId method is not valid.");
+            var model = _repository.GetPhonesByUserId(userId);
+            return PartialView(model);
+        }
+    }
+}
