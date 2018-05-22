01ae5b9bee3d2a773aa5132d49ec5e77cbf05ab7
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-22 08:27:49 +0300

    hf-1.12.8 -- add LocationRepository with GetLocationByCityId method

---
+++ Callboard/Callboard.App.Data/Repositories/Abstract/Location/ILocationRepository.cs
@@ -0,0 +1,9 @@
+﻿using Callboard.App.General.Entities;
+
+namespace Callboard.App.Data.Repositories
+{
+    public interface ILocationRepository : IEntityRepository<Location>
+    {
+        Location GetLocationByCityId(int cityId);
+    }
+}
---
+++ Callboard/Callboard.App.Data/Repositories/Realizations/Location/LocationRepository.cs
@@ -0,0 +1,35 @@
+﻿using Callboard.App.Data.Infrastructure;
+using Callboard.App.General.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace Callboard.App.Data.Repositories
+{
+    internal class LocationRepository : EntityRepository, ILocationRepository
+    {
+        private const string TABLE_NAME = "Location";
+        public LocationRepository()
+            : base() { }
+
+        protected override string TableName => TABLE_NAME;
+
+        public IReadOnlyCollection<Location> Items => throw new NotImplementedException();
+
+        public Location GetLocationByCityId(int cityId)
+        {
+            Location location = base.GetEntity<Location>("getLocationById", MapLocation, cityId);
+            return location;
+        }
+
+        private Location MapLocation(DbDataReader reader)
+        {
+            return new Location
+            {
+                Area = Mapper.MapProperty<string>(reader, "Area", base.GetName),
+                City = Mapper.MapProperty<string>(reader, "City", base.GetName),
+                Country = Mapper.MapProperty<string>(reader, "Country", base.GetName)
+            };
+        }
+    }
+}
