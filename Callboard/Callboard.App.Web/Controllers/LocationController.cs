d0ec31f3f49e79c5b21b04ff1e3bb3ed2baea084
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-22 09:32:20 +0300

    hf-1.13.10 -- add User and Location controllers

---
+++ Callboard/Callboard.App.Web/Controllers/LocationController.cs
@@ -0,0 +1,23 @@
+﻿using Callboard.App.Business.Repositories;
+using Callboard.App.General.Entities;
+using Callboard.App.General.Helpers;
+using System.Web.Mvc;
+
+namespace Callboard.App.Web.Controllers
+{
+    public class LocationController : Controller
+    {
+        private ILocationRepository _repository;
+        public LocationController(ILocationRepository repository)
+        {
+            Checker.CheckForNull(repository, $"Cannot find { typeof(ILocationRepository) } in App.Web LocationController");
+            _repository = repository;
+        }
+
+        public PartialViewResult GetLocationByCityId(int cityId)
+        {
+            Location location = _repository.GetLocationByCityId(cityId);
+            return PartialView("Location", location);
+        }
+    }
+}
---
+++ Callboard/Callboard.App.Web/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Callboard.App.Business.Repositories;
+using Callboard.App.General.Helpers;
+using System.Web.Mvc;
+
+namespace Callboard.App.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserRepository _repository;
+        public UserController(IUserRepository repository)
+        {
+            Checker.CheckForNull(repository, $"Cannot find { typeof(IUserRepository) } in App.Web UserController");
+            _repository = repository;
+        }
+
+        public PartialViewResult GetUser(int userId)
+        {
+            var user = _repository.GetUserById(userId);
+            return PartialView("User", user);
+        }
+    }
+}
