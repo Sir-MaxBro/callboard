ba441d5a70176c4e86570e9494192762e08573ea
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-23 13:07:47 +0300

    hf-1.21.4 -- add CountryRepository realization in App.Business

---
+++ Callboard/Callboard.App.Business/Repositories/Realizations/Location/CountryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Callboard.App.Business.DependencyResolution;
+using Callboard.App.General.Entities;
+using System.Collections.Generic;
+using Data = Callboard.App.Data.Repositories;
+
+namespace Callboard.App.Business.Repositories
+{
+    internal class CountryRepository : ICountryRepository
+    {
+        private Data::ICountryRepository _repository;
+        public CountryRepository()
+        {
+            _repository = DataContainer.GetInstance<Data::ICountryRepository>();
+        }
+
+        public IReadOnlyCollection<Country> Items
+        {
+            get => _repository.Items;
+        }
+    }
+}
