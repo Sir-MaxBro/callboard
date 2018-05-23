8ab93a8171943ebd122556a6a227bdb268f0ec09
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-23 16:48:13 +0300

    hf-1.24.2 -- add ajax-helper script

---
+++ Callboard/Callboard.App.Web/Scripts/ajax-helper.js
@@ -0,0 +1,14 @@
+﻿function executeAction(obj, url, updateTargetId) {
+    $.ajax({
+        url: url,
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(obj),
+        success: function (data) {
+            $('#' + updateTargetId).html(data);
+        },
+        error: function () {
+            alert('Action by url = ' + url + 'not found');
+        }
+    });
+}
