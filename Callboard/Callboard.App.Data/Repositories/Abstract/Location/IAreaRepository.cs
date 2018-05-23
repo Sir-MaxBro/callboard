2159b9dde6d89a19ed61ea05b79d0d9663d1bf63
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-23 13:29:02 +0300

    hf-1.22.1 -- add AreaRepository in App.Data

---
+++ Callboard/Callboard.App.Data/Repositories/Abstract/Location/IAreaRepository.cs
@@ -0,0 +1,10 @@
+﻿using Callboard.App.General.Entities;
+using System.Collections.Generic;
+
+namespace Callboard.App.Data.Repositories
+{
+    public interface IAreaRepository : IEntityRepository<Area>
+    {
+        IReadOnlyCollection<Area> GetAreasByCountryId(int countryId);
+    }
+}
---
+++ Callboard/Callboard.App.Data/Repositories/Realizations/Location/AreaRepository.cs
@@ -0,0 +1,35 @@
+﻿using Callboard.App.Data.Infrastructure;
+using Callboard.App.General.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace Callboard.App.Data.Repositories
+{
+    internal class AreaRepository : EntityRepository, IAreaRepository
+    {
+        private const string TABLE_NAME = "Area";
+        public AreaRepository()
+            : base() { }
+
+        protected override string TableName => TABLE_NAME;
+
+        IReadOnlyCollection<Area> IEntityRepository<Area>.Items => throw new NotImplementedException();
+
+        public IReadOnlyCollection<Area> GetAreasByCountryId(int countryId)
+        {
+            IReadOnlyCollection<Area> areas = base.GetEntities<Area>("selectByCountryId", MapArea, countryId);
+            return areas;
+        }
+
+        private Area MapArea(DbDataReader reader)
+        {
+            return new Area
+            {
+                AreaId = Mapper.MapProperty<int>(reader, "AreaId", base.GetName),
+                CountryId = Mapper.MapProperty<int>(reader, "CountryId", base.GetName),
+                Name = Mapper.MapProperty<string>(reader, "Name", base.GetName)
+            };
+        }
+    }
+}
