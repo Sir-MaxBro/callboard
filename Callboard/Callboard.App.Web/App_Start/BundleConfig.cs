39e2c938525557a78373f1501f22c0a1b8953b0b
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-23 16:48:46 +0300

    hf-1.24.3 -- add bundle for ajax libraries

--- Callboard/Callboard.App.Web/App_Start/BundleConfig.cs
+++ Callboard/Callboard.App.Web/App_Start/BundleConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Callboard.App.Web
 {
@@ -12,6 +8,11 @@ namespace Callboard.App.Web
         {
             bundles.Add(new StyleBundle("~/bundles/css")
                 .IncludeDirectory("~/Content/css", "*.css", true));
+
+            bundles.Add(new ScriptBundle("~/bundles/ajax")
+                .Include("~/Scripts/jquery-1.10.2.min.js")
+                .Include("~/Scripts/jquery.unobtrusive-ajax.min.js")
+                .Include("~/Scripts/ajax-helper.js"));
         }
     }
 }
