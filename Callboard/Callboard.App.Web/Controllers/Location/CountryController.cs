a0aed9ed88c6fe5c72a46f8e93dc27ae4ff96c0a
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-23 13:08:49 +0300

    hf-1.21.6 -- add CountryController

---
+++ Callboard/Callboard.App.Web/Controllers/Location/CountryController.cs
@@ -0,0 +1,24 @@
+﻿using Callboard.App.Business.Repositories;
+using Callboard.App.General.Helpers;
+using Newtonsoft.Json;
+using System.Web.Mvc;
+
+namespace Callboard.App.Web.Controllers
+{
+    public class CountryController : Controller
+    {
+        private ICountryRepository _repository;
+        public CountryController(ICountryRepository repository)
+        {
+            Checker.CheckForNull(repository, $"Cannot find { typeof(ICountryRepository) } in App.Web CountryController");
+            _repository = repository;
+        }
+
+        public JsonResult GetCountries()
+        {
+            var countries = _repository.Items;
+            var countriesData = JsonConvert.SerializeObject(countries);
+            return Json(countriesData, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
