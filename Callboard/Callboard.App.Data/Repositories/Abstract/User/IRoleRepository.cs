77edc37bef4cbd67eab347a100ca826317076256
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-24 16:28:12 +0300

    hf-1.30.3 -- add IRoleRepository abstract

---
+++ Callboard/Callboard.App.Data/Repositories/Abstract/User/IRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using Callboard.App.General.Entities;
+using System.Collections.Generic;
+
+namespace Callboard.App.Data.Repositories
+{
+    public interface IRoleRepository : IEntityRepository<Role>
+    {
+        IReadOnlyCollection<Role> GetRolesByUserId(int userId);
+    }
+}
