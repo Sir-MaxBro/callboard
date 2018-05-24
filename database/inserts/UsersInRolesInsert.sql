b6d7dfb68d0a40706d193fad6c077ec60c3b2a10
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-24 15:59:21 +0300

    hf-1.29.3 -- add UsersInRolesInsert

---
+++ database/inserts/UsersInRolesInsert.sql
@@ -0,0 +1,5 @@
+USE [callboardDB];
+INSERT INTO [dbo].[UsersInRoles]([UserId], [RoleId]) VALUES(1, 1)
+INSERT INTO [dbo].[UsersInRoles]([UserId], [RoleId]) VALUES(2, 2)
+INSERT INTO [dbo].[UsersInRoles]([UserId], [RoleId]) VALUES(3, 3)
+INSERT INTO [dbo].[UsersInRoles]([UserId], [RoleId]) VALUES(4, 2)
