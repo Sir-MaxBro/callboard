d1ce91f920d2e5a85237a9ade05f34fee1a56ce3
Sir-MaxBro <maxbrofenx@gmail.com>
2018-05-24 10:53:39 +0300

    hf-1.28.1 -- add getDataAsync method in ajax-helper

--- Callboard/Callboard.App.Web/Scripts/ajax-helper.js
+++ Callboard/Callboard.App.Web/Scripts/ajax-helper.js
@@ -4,8 +4,24 @@
         type: 'POST',
         contentType: 'application/json',
         data: JSON.stringify(obj),
+        dataType: "html",
         success: function (data) {
             $('#' + updateTargetId).html(data);
+        },
+        error: function () {
+            $('#' + updateTargetId).html("Not found");
         }
+    });
+}
+
+function getDataAsync(obj, url, successFunc, errorFunc) {
+    $.ajax({
+        url: url,
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(obj),
+        dataType: "json",
+        success: successFunc,
+        error: errorFunc
     });
 }
