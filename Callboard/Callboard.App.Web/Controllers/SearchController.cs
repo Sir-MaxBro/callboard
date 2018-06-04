1e7f533e60927c00d8629b42830e67ad52aa82da
Sir-MaxBro <maxbrofenx@gmail.com>
2018-06-04 14:51:33 +0300

    cb-6.1.6 -- add SearchControlles

---
+++ Callboard/Callboard.App.Web/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using Callboard.App.Business.Providers.Main;
+using Callboard.App.General.Helpers.Main;
+using Callboard.App.General.Loggers.Main;
+using Callboard.App.Web.Models;
+using System;
+using System.Web.Mvc;
+
+namespace Callboard.App.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private IAdProvider _adProvider;
+        private IChecker _checker;
+        private ILoggerWrapper _logger;
+        public SearchController(IAdProvider adProvider, ILoggerWrapper logger, IChecker checker)
+        {
+            if (checker == null)
+            {
+                throw new NullReferenceException(nameof(checker));
+            }
+            _checker = checker;
+            _checker.CheckForNull(logger);
+            _checker.CheckForNull(adProvider);
+            _logger = logger;
+            _adProvider = adProvider;
+        }
+
+        public ActionResult SearchAdsByName(string name)
+        {
+            SearchViewModel model = new SearchViewModel
+            {
+                Ads = _adProvider.SearchByName(name)
+            };
+            return View("Search", model);
+        }
+    }
+}
---
+++ Callboard/Callboard.App.Web/Models/SearchViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Callboard.App.General.Entities;
+using System.Collections.Generic;
+
+namespace Callboard.App.Web.Models
+{
+    public class SearchViewModel
+    {
+        public IReadOnlyCollection<Ad> Ads { get; set; }
+    }
+}
