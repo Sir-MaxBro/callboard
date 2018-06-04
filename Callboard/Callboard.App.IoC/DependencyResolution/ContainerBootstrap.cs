8d98349a6f996a9b7696a014e878bd4d2e2335d3
Sir-MaxBro <maxbrofenx@gmail.com>
2018-06-04 10:59:38 +0300

    hf-1.33.1 -- add all dependency in IoC

--- Callboard/Callboard.App.IoC/DependencyResolution/ContainerBootstrap.cs
+++ Callboard/Callboard.App.IoC/DependencyResolution/ContainerBootstrap.cs
@@ -1,6 +1,13 @@
-﻿using Callboard.App.Business.Repositories;
-using Callboard.App.General.Loggers;
+﻿using Callboard.App.Business.Providers.Main;
+using Callboard.App.Business.Services;
+using Callboard.App.Data.Context.Main;
+using Callboard.App.Data.DataProviders.Main;
+using Callboard.App.Data.Repositories.Main;
+using Callboard.App.General.Cache.Main;
+using Callboard.App.General.Helpers.Main;
+using Callboard.App.General.Loggers.Main;
 using StructureMap;
+using StructureMap.Graph;
 
 namespace Callboard.App.IoC.DependencyResolution
 {
@@ -13,13 +20,45 @@ namespace Callboard.App.IoC.DependencyResolution
                 x => x.Scan
                        (
                            scan =>
-                           {
-                               scan.AssemblyContainingType<IEntityRepository<object>>();
-                               scan.AssemblyContainingType<ILoggerWrapper>();
+                           {                              
+                               AddDataDependency(scan);
+                               AddBusinessDependency(scan);
+                               AddGeneralDependency(scan);
                                scan.LookForRegistries();
                            }
                        )
           );
+        }
+
+        private static void AddGeneralDependency(IAssemblyScanner scan)
+        {
+            scan.AssemblyContainingType<ILoggerWrapper>();
+            scan.AssemblyContainingType<IChecker>();
+            scan.AssemblyContainingType<ICacheStorage>();
+        }
+
+        private static void AddBusinessDependency(IAssemblyScanner scan)
+        {
+            scan.AssemblyContainingType<ILogginService>();
+            scan.AssemblyContainingType<IEntityProvider<object>>();
+        }
+
+        private static void AddDataDependency(IAssemblyScanner scan)
+        {
+            scan.AssemblyContainingType<ISqlDataProvider<object>>();
+            scan.AssemblyContainingType<ISqlDbContext>();
+
+            scan.AssemblyContainingType<ICategoryRepository>();
+            scan.AssemblyContainingType<IUserRepository>();
+            scan.AssemblyContainingType<IRoleRepository>();
+            scan.AssemblyContainingType<IAreaRepository>();
+            scan.AssemblyContainingType<ICityRepository>();
+            scan.AssemblyContainingType<ICountryRepository>();
+
+            scan.AssemblyContainingType<Data.Providers.Main.IAdProvider>();
+            scan.AssemblyContainingType<Data.Providers.Main.IMembershipProvider>();
+            scan.AssemblyContainingType<Data.Providers.Main.ICommercialProvider>();
+            scan.AssemblyContainingType<Data.Providers.Main.IAdDetailsProvider>();
         }
 
         public static IContainer Container
