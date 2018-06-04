f20dc58ef194983098b969d318ab0f187e281d4c
Sir-MaxBro <maxbrofenx@gmail.com>
2018-06-04 17:37:11 +0300

    cb-6.1.10 -- add kind repository in App.Data

---
+++ Callboard/Callboard.App.Data/Repositories/Main/IKindRepository.cs
@@ -0,0 +1,8 @@
+﻿using Callboard.App.General.Entities;
+
+namespace Callboard.App.Data.Repositories.Main
+{
+    public interface IKindRepository : IRepository<Kind>
+    {
+    }
+}
---
+++ Callboard/Callboard.App.Data/Repositories/Realizations/Sql/KindSqlRepository.cs
@@ -0,0 +1,57 @@
+﻿using Callboard.App.Data.DataProviders.Main;
+using Callboard.App.Data.Mappers;
+using Callboard.App.Data.Providers.Main;
+using Callboard.App.Data.Repositories.Main;
+using Callboard.App.General.Entities;
+using Callboard.App.General.Helpers.Main;
+using Callboard.App.General.Loggers.Main;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Callboard.App.Data.Repositories.Realizations.Sql
+{
+    internal class KindSqlRepository : SqlProvider<Kind>, IKindRepository
+    {
+        public KindSqlRepository(ISqlDataProvider<Kind> sqlProvider, ILoggerWrapper logger, IChecker checker) 
+            : base(sqlProvider, logger, checker) { }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<Kind> GetAll()
+        {
+            string procedureName = "sp_select_kind";
+            var mapper = new Mapper<DataSet, Kind> { MapCollection = this.MapKindCollection };
+            var kinds = base.GetAll(procedureName, mapper);
+            return kinds;
+        }
+
+        public Kind GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Kind obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IReadOnlyCollection<Kind> MapKindCollection(DataSet dataSet)
+        {
+            return dataSet.Tables[0].AsEnumerable().Select(this.MapKind).ToList();
+        }
+
+        private Kind MapKind(DataRow row)
+        {
+            return new Kind
+            {
+                KindId = row.Field<int>("KindId"),
+                Type = row.Field<string>("Type")
+            };
+        }
+    }
+}
