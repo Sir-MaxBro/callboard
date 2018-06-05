837c48e79da76fed13f042112a0b16b0a23f8ebd
Sir-MaxBro <maxbrofenx@gmail.com>
2018-06-05 13:52:20 +0300

    cb-6.2.3 -- add SearchConfiguration model in App.General

---
+++ Callboard/Callboard.App.General/Entities/Data/Ad/SearchConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace Callboard.App.General.Entities.Data.Ad
+{
+    public class SearchConfiguration
+    {
+        public string Name { get; set; }
+
+        public string State { get; set; }
+
+        public string Kind { get; set; }
+
+        public decimal MinPrice { get; set; }
+
+        public decimal MaxPrice { get; set; }
+
+        public int CoutryId { get; set; }
+
+        public int AreaId { get; set; }
+
+        public int CityId { get; set; }
+    }
+}
