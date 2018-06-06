61002acf376555eda82fb7b7d33380b53b8b8f7e
Sir-MaxBro <maxbrofenx@gmail.com>
2018-06-06 18:39:52 +0300

    cb-6.4.2 -- add in SearchViewModel SearchConfiguration field

--- Callboard/Callboard.App.Web/Models/SearchViewModel.cs
+++ Callboard/Callboard.App.Web/Models/SearchViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Callboard.App.General.Entities;
+using Callboard.App.General.Entities.Data;
 using System.Collections.Generic;
 
 namespace Callboard.App.Web.Models
@@ -6,5 +7,7 @@ namespace Callboard.App.Web.Models
     public class SearchViewModel
     {
         public IReadOnlyCollection<Ad> Ads { get; set; }
+
+        public SearchConfiguration SearchConfiguration { get; set; }
     }
 }
