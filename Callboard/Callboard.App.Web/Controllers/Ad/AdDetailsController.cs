d4df4673cf02ededc00ce8b0424c331e28ba3df4
Sir-MaxBro <maxbrofenx@gmail.com>
2018-06-07 15:36:58 +0300

    cb-6.6.4 -- add in AdDetailsController edit and save methods

--- Callboard/Callboard.App.Web/Controllers/Ad/AdDetailsController.cs
+++ Callboard/Callboard.App.Web/Controllers/Ad/AdDetailsController.cs
@@ -1,7 +1,11 @@
 ﻿using Callboard.App.Business.Providers.Main;
 using Callboard.App.General.Entities;
 using Callboard.App.General.Helpers.Main;
+using Callboard.App.Web.Attributes;
+using Callboard.App.Web.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Callboard.App.Web.Controllers
@@ -26,6 +30,65 @@ namespace Callboard.App.Web.Controllers
             ViewBag.ReturnUrl = returnUrl;
             AdDetails model = _adDetailsProvider.GetById(adId);
             return View("AdDetails", model);
+        }
+
+        [User]
+        public ActionResult EditAdDetails(int adId)
+        {
+            var adDetails = _adDetailsProvider.GetById(adId);
+            var adDetailsModel = this.MapAdDetailsToViewModel(adDetails);
+            return View("EditAdDetails", adDetailsModel);
+        }
+
+        [User]
+        [HttpPost]
+        public ActionResult SaveAdDetails(AdDetailsViewModel adDetailsModel)
+        {
+            if (adDetailsModel != null)
+            {
+                var adDetails = this.MapViewModelToAdDetails(adDetailsModel);
+                _adDetailsProvider.Save(adDetails);
+                return RedirectToAction("GetAdDetails", new { adId = adDetails.AdId });
+            }
+            return RedirectToAction("Error", "Error");
+        }
+
+        private AdDetails MapViewModelToAdDetails(AdDetailsViewModel adDetailsModel)
+        {
+            return new AdDetails
+            {
+                AdId = adDetailsModel.AdId,
+                Name = adDetailsModel.Name,
+                State = adDetailsModel.State,
+                Kind = adDetailsModel.Kind,
+                Price = adDetailsModel.Price,
+                Description = adDetailsModel.Description,
+                Location = new General.Entities.Location { LocationId = adDetailsModel.LocationId },
+                User = new User { UserId = adDetailsModel.UserId },
+                AddressLine = adDetailsModel.AddressLine,
+                CreationDate = adDetailsModel.CreationDate,
+                Categories = (IReadOnlyCollection<Category>)adDetailsModel.Categories,
+                Images = (IReadOnlyCollection<Image>)adDetailsModel.Images
+            };
+        }
+
+        private AdDetailsViewModel MapAdDetailsToViewModel(AdDetails adDetails)
+        {
+            return new AdDetailsViewModel
+            {
+                AdId = adDetails.AdId,
+                Name = adDetails.Name,
+                State = adDetails.State,
+                Kind = adDetails.Kind,
+                Price = adDetails.Price,
+                Description = adDetails.Description,
+                LocationId = adDetails.Location.LocationId,
+                UserId = adDetails.User.UserId,
+                AddressLine = adDetails.AddressLine,
+                CreationDate = adDetails.CreationDate,
+                Categories = adDetails.Categories.ToList(),
+                Images = adDetails.Images.ToList()
+            };
         }
     }
 }
