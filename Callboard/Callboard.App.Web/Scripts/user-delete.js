f8e5c6d3ee8be4a31177cda5142daf5f347a7a9d
Sir-MaxBro <maxbrofenx@gmail.com>
2018-06-07 10:30:25 +0300

    cb-6.5.3 -- add js script for delete user in App.Web

---
+++ Callboard/Callboard.App.Web/Scripts/user-delete.js
@@ -0,0 +1,14 @@
+﻿var updateTargetId;
+
+function deleteUser(id, updateId) {
+    updateTargetId = updateId;
+    getDataAsync({ userId: id }, '/User/DeleteUserById', userDeleted);
+}
+
+var userDeleted = function (data) {
+    var isDeleted = JSON.parse(data.IsDeleted);
+    console.log(updateTargetId);
+    if (isDeleted) {
+        $("#" + updateTargetId).remove();
+    }
+}
