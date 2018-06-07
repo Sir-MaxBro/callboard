966913638db737011fcf1b14e5c652045554a802
Sir-MaxBro <maxbrofenx@gmail.com>
2018-06-07 13:58:11 +0300

    cb-6.5.10 -- add SaveUser and EditUser methods in UserController

--- Callboard/Callboard.App.Web/Controllers/User/UserController.cs
+++ Callboard/Callboard.App.Web/Controllers/User/UserController.cs
@@ -1,7 +1,11 @@
 ﻿using Callboard.App.Business.Providers.Main;
+using Callboard.App.General.Entities;
 using Callboard.App.General.Helpers.Main;
 using Callboard.App.Web.Attributes;
+using Callboard.App.Web.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Callboard.App.Web.Controllers
@@ -27,6 +31,34 @@ namespace Callboard.App.Web.Controllers
             return PartialView("User", user);
         }
 
+        [User]
+        public ActionResult OpenProfile(int userId)
+        {
+            var user = _userProvider.GetById(userId);
+            return View("UserProfile", user);
+        }
+
+        [User]
+        public ActionResult EditUser(int userId)
+        {
+            var user = _userProvider.GetById(userId);
+            var userModel = this.MapUserToViewModel(user);
+            return View("EditUser", userModel);
+        }
+
+        [User]
+        [HttpPost]
+        public ActionResult SaveUser(UserViewModel userModel)
+        {
+            if (userModel != null)
+            {
+                var user = this.MapViewModelToUser(userModel);
+                _userProvider.Save(user);
+                return RedirectToAction("OpenProfile", user);
+            }
+            return RedirectToAction("Error", "Error");
+        }
+
         [Admin]
         public ActionResult GetAllUser()
         {
@@ -40,6 +72,32 @@ namespace Callboard.App.Web.Controllers
         {
             _userProvider.Delete(userId);
             return Json(new { IsDeleted = true });
+        }
+
+        private UserViewModel MapUserToViewModel(User user)
+        {
+            return new UserViewModel
+            {
+                UserId = user.UserId,
+                Name = user.Name,
+                PhotoData = user.PhotoData,
+                PhotoExtension = user.PhotoExtension,
+                Phones = user.Phones.ToList(),
+                Mails = user.Mails.ToList()
+            };
+        }
+
+        private User MapViewModelToUser(UserViewModel userModel)
+        {
+            return new User
+            {
+                UserId = userModel.UserId,
+                Name = userModel.Name,
+                PhotoData = userModel.PhotoData,
+                PhotoExtension = userModel.PhotoExtension,
+                Mails = (IReadOnlyCollection<Mail>)userModel.Mails,
+                Phones = (IReadOnlyCollection<Phone>)userModel.Phones
+            };
         }
     }
 }
