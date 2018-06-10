34108194c1f5b0581e87d04b3427c3c8377a9b04
Sir-MaxBro <maxbrofenx@gmail.com>
2018-06-10 14:54:25 +0300

    cb-7.3.4 -- add RoleController

---
+++ Callboard/Callboard.App.Web/Controllers/User/RoleController.cs
@@ -0,0 +1,57 @@
+﻿using Callboard.App.Business.Providers.Main;
+using Callboard.App.General.Helpers.Main;
+using Callboard.App.Web.Attributes;
+using Newtonsoft.Json;
+using System;
+using System.Web.Mvc;
+
+namespace Callboard.App.Web.Controllers
+{
+    public class RoleController : Controller
+    {
+        private IRoleProvider _roleProvider;
+        private IChecker _checker;
+        public RoleController(IRoleProvider roleProvider, IChecker checker)
+        {
+            if (checker == null)
+            {
+                throw new NullReferenceException(nameof(checker));
+            }
+            _checker = checker;
+            _checker.CheckForNull(roleProvider);
+            _roleProvider = roleProvider;
+        }
+
+        [Admin]
+        public JsonResult GetRoles()
+        {
+            var roles = _roleProvider.GetAll();
+            var rolesData = JsonConvert.SerializeObject(roles);
+            return Json(new { Roles = rolesData }, JsonRequestBehavior.AllowGet);
+        }
+
+        [Admin]
+        public JsonResult GetRolesForUser(int userId)
+        {
+            var roles = _roleProvider.GetRolesForUser(userId);
+            var rolesData = JsonConvert.SerializeObject(roles);
+            return Json(new { Roles = rolesData }, JsonRequestBehavior.AllowGet);
+        }
+
+        [Admin]
+        [HttpPost]
+        public ActionResult SetRoleForUser(int userId, int roleId)
+        {
+            _roleProvider.SetRoleForUser(userId, roleId);
+            return RedirectToAction("GetRolesForUser", new { userId });
+        }
+
+        [Admin]
+        [HttpPost]
+        public ActionResult DeleteUserRole(int userId, int roleId)
+        {
+            _roleProvider.DeleteUserRole(userId, roleId);
+            return RedirectToAction("GetRolesForUser", new { userId });
+        }
+    }
+}
