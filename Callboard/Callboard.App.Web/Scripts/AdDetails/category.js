5645a81c3630b93130bb869f372011c2df1cc050
Sir-MaxBro <maxbrofenx@gmail.com>
2018-06-11 11:13:40 +0300

    cb-7.4.13 -- add category js scripts for EditAdDetails

---
+++ Callboard/Callboard.App.Web/Scripts/AdDetails/category.js
@@ -0,0 +1,13 @@
+﻿function loadCategories() {
+    getDataAsync(null, '/Category/GetCategories', renderCategories);
+}
+
+let renderCategories = function (data) {
+    let categories = JSON.parse(data.Categories);
+    let categoryContainer = $("#categories");
+    categoryContainer.empty();
+    for (let i = 0; i < categories.length; i++) {
+        let option = createOption('Categories[' + i + '].CategoryId', categories[i].CategoryId, categories[i].Name);
+        categoryContainer.append(option);
+    }
+}
