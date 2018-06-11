e971b0bf1d3eb201b3220a5e40a77f088a718781
Sir-MaxBro <maxbrofenx@gmail.com>
2018-06-11 23:15:12 +0300

    cb-7.5.8 -- add in addetails-save.js fill images logic

--- Callboard/Callboard.App.Web/Scripts/AdDetails/addetails-save.js
+++ Callboard/Callboard.App.Web/Scripts/AdDetails/addetails-save.js
@@ -1,7 +1,6 @@
 ﻿function saveAdDetails() {
     let adDetails = getAdDetails();
     console.log(adDetails);
-    console.log({ adDetailsModel: JSON.stringify(adDetails) });
     $.post('/AdDetails/SaveAdDetails', { adDetailsData: JSON.stringify(adDetails) });
 }
 
@@ -14,15 +13,10 @@ let getAdDetails = function () {
     let state = $("#states").find(":selected").val();
     let userId = $("#userId").val();
     let adId = $("#adId").val();
-    let locationId = $("#cities").find(":selected").val();
 
-    let categories = [];
-    $("#categories").find(":selected").each(function () {
-        let category = {
-            CategoryId: $(this).val()
-        };
-        categories.push(category);
-    });
+    let locationId = getLocationId();
+    let categories = getCategoriesList();
+    let images = getImages();
 
     return {
         Name: name,
@@ -33,7 +27,55 @@ let getAdDetails = function () {
         State: state,
         UserId: userId,
         AdId: adId,
-        LocationId: locationId,
-        Categories: categories
+        Location: {
+            LocationId: locationId
+        },
+        Categories: categories,
+        Images: images
     };
+}
+
+let getLocationId = function () {
+    let locationId = $("#cities").find(":selected").val();
+    if (typeof locationId != 'undefined') {
+        return locationId;
+    }
+    else {
+        return $("#locationId").val();
+    }
+}
+
+let getCategoriesList = function () {
+    let categories = [];
+    $("#categories").find(":selected").each(function () {
+        let category = {
+            CategoryId: $(this).val()
+        };
+        categories.push(category);
+    });
+    return categories;
+}
+
+let getImages = function () {
+    let images = [];
+    $("#images").find("img").each(function () {
+        let imageSrc = this.src;
+
+        let imageData = imageSrc.split(";");
+        let extension = imageData[0].split(":")[1].split("/")[1];
+        let imageBase64 = imageData[1].split(",")[1];
+
+        let imageId = $(this).data('imageId');
+        if (typeof imageId == 'undefined') {
+            imageId = 0;
+        }
+
+        let image = {
+            ImageId: imageId,
+            Data: convertBase64ToByte(imageBase64),
+            Extension: extension
+        };
+        images.push(image);
+    });
+    return images;
 }
