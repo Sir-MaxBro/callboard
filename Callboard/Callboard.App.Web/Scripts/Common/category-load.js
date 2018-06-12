ab36156e9d43124520d6c1393b0ca794f40decd9
Maksim Chendrov <Maksim_Chendrov@epam.com>
2018-06-12 12:42:58 +0300

    cb-7.5.11 -- add common folder for js scripts

---
+++ Callboard/Callboard.App.Web/Scripts/Common/category-load.js
@@ -0,0 +1,34 @@
+﻿let selectedCategories;
+let categotyUpdateTargetId;
+
+function loadCategories(updateTargetId, selectedCategoriesData) {
+    selectedCategories = selectedCategoriesData;
+    categotyUpdateTargetId = updateTargetId;
+    getDataAsync(null, '/Category/GetCategories', renderCategories);
+}
+
+let renderCategories = function (data) {
+    let categories = JSON.parse(data.Categories);
+    let categoryContainer = $("#" + categotyUpdateTargetId);
+    categoryContainer.empty();
+    for (let i = 0; i < categories.length; i++) {
+        let option = createOption('Categories[' + i + '].CategoryId', categories[i].CategoryId, categories[i].Name);
+
+        if (isSelected(categories[i].CategoryId)) {
+            option.attr('selected', 'selected');
+        }
+
+        categoryContainer.append(option);
+    }
+}
+
+let isSelected = function (categoryId) {
+    if (typeof selectedCategories != 'undefined' && selectedCategories != null) {
+        for (let i = 0; i < selectedCategories.length; i++) {
+            if (selectedCategories[i].CategoryId == categoryId) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
---
+++ Callboard/Callboard.App.Web/Scripts/Common/common.js
@@ -0,0 +1,33 @@
+﻿function createOption(name, value, text) {
+    let option = $('<option></option>');
+    option.attr('name', name);
+    option.attr('value', value);
+    option.text(text);
+    return option;
+}
+
+function renderLink(text, click) {
+    let link = $('<a></a>');
+    link.attr('href', '#');
+    link.text(text);
+    link.on('click', function () {
+        click.call();
+    });
+    return link;
+}
+
+function convertByteToBase64(byteArray) {
+    var base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(byteArray)));
+    return base64String;
+}
+
+function convertBase64ToByte(base64) {
+    var binaryString = window.atob(base64);
+    var length = binaryString.length;
+    var bytes = new Uint8Array(length);
+    for (var i = 0; i < length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    let byteArray = [].slice.call(bytes);
+    return byteArray;
+}
