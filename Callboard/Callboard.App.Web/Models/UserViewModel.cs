ef33831cbf0700d4857d9ada2cac6f574ccda18e
Maksim Chendrov <Maksim_Chendrov@epam.com>
2018-06-12 17:17:59 +0300

    cb-7.6.1 -- change UserViewModel __ add User property

--- Callboard/Callboard.App.Web/Models/UserViewModel.cs
+++ Callboard/Callboard.App.Web/Models/UserViewModel.cs
@@ -1,26 +1,9 @@
 ﻿using Callboard.App.General.Entities;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Web.Mvc;
 
 namespace Callboard.App.Web.Models
 {
     public class UserViewModel
     {
-        [Required]
-        [HiddenInput(DisplayValue = false)]
-        public int UserId { get; set; }
-
-        [Required]
-        [MinLength(5)]
-        public string Name { get; set; }
-
-        public byte[] PhotoData { get; set; }
-
-        public string PhotoExtension { get; set; }
-
-        public IList<Phone> Phones { get; set; }
-
-        public IList<Mail> Mails { get; set; }
+        public User User { get; set; }
     }
 }
