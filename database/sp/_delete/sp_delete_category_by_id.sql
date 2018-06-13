779e9d07745991880f6a8b0c5fd8cc93dbc804ee
Maksim Chendrov <Maksim_Chendrov@epam.com>
2018-06-13 11:17:21 +0300

    cb-7.10.3 -- change sql delete category __ delete all subcategories

--- database/sp/_delete/sp_delete_category_by_id.sql
+++ database/sp/_delete/sp_delete_category_by_id.sql
@@ -4,8 +4,16 @@ CREATE PROCEDURE [dbo].[sp_delete_category_by_id]
 	@CategoryId INT
 AS
 BEGIN
+    DECLARE @Subcategories TABLE ([CategoryId] INT, [ParentId] INT, [Level] VARCHAR(MAX))
+	INSERT INTO @Subcategories
+	EXEC [dbo].[sp_select_all_subcategories_by_categoryid] @CategoryId
+
     DELETE 
     FROM [dbo].[Category]
     WHERE [Category].[CategoryId] = @CategoryId
+    OR [Category].[CategoryId] IN ( 
+		SELECT [categories].[CategoryId] 
+		FROM @Subcategories AS [categories]
+	)
 END
 GO
