57344d274d7683e94ebb6a84366c961de740864f
Maksim Chendrov <Maksim_Chendrov@epam.com>
2018-06-20 18:18:00 +0300

    hf-2.30.1 -- filled the database with countries, areas and cities, also change user logins and passwords

--- database/inserts/AreaInsert.sql
+++ database/inserts/AreaInsert.sql
@@ -1,4 +1,11 @@
 USE [callboardDB];
-INSERT INTO [dbo].[Area]([CountryId], [Name]) VALUES(1, "Mogilev region")
-INSERT INTO [dbo].[Area]([CountryId], [Name]) VALUES(2, "Moscow region")
+--Belarus
+INSERT INTO [dbo].[Area]([CountryId], [Name]) VALUES(1, "Brest region")
+INSERT INTO [dbo].[Area]([CountryId], [Name]) VALUES(1, "Gomel region")
 INSERT INTO [dbo].[Area]([CountryId], [Name]) VALUES(1, "Grodno region")
+INSERT INTO [dbo].[Area]([CountryId], [Name]) VALUES(1, "Minsk region")
+INSERT INTO [dbo].[Area]([CountryId], [Name]) VALUES(1, "Mogilev region")
+INSERT INTO [dbo].[Area]([CountryId], [Name]) VALUES(1, "Vitebsk region")
+--Russia
+INSERT INTO [dbo].[Area]([CountryId], [Name]) VALUES(2, "Moscow region")
+INSERT INTO [dbo].[Area]([CountryId], [Name]) VALUES(2, "Smolensk region")
