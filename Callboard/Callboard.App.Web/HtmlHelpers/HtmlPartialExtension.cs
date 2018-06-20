a9f8acfb117c2254a5b20e395fbf60e30cd163cf
Maksim Chendrov <Maksim_Chendrov@epam.com>
2018-06-20 11:12:36 +0300

    hf-2.26.2 -- added HtmlHelper for getting PartialView as html string

---
+++ Callboard/Callboard.App.Web/HtmlHelpers/HtmlPartialExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Callboard.App.Web.HtmlHelpers
+{
+    public static class HtmlPartialExtension
+    {
+        public static IHtmlString GetHtmlPartial(this HtmlHelper htmlHelper, string partialViewName, object model)
+        {
+            IDictionary<string, string> removedCharacters = new Dictionary<string, string>
+            {
+                { "\n", "" },
+                { "\r", "" },
+                { "\"", "\\\"" },
+                { "\'", "\\\'" }
+            };
+
+            string partialView = System.Web.Mvc.Html.PartialExtensions.Partial(htmlHelper, partialViewName, model).ToString();
+
+            StringBuilder partialViewHtml = new StringBuilder(partialView);
+
+            foreach (var character in removedCharacters)
+            {
+                partialViewHtml.Replace(character.Key, character.Value);
+            }
+
+            return MvcHtmlString.Create(partialViewHtml.ToString());
+        }
+    }
+}
