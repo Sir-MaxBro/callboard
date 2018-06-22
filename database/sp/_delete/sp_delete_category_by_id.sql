d4d390b4e32ef0d588a4992569e1a2dc79d78a6d
Maksim Chendrov <Maksim_Chendrov@epam.com>
2018-06-22 10:41:08 +0300

    cb-9.1.8 -- added transaction for delete category and images

--- database/sp/_delete/sp_delete_category_by_id.sql
+++ database/sp/_delete/sp_delete_category_by_id.sql
@@ -4,6 +4,8 @@ CREATE PROCEDURE [dbo].[sp_delete_category_by_id]
 	@CategoryId INT
 AS
 BEGIN
+BEGIN TRANSACTION;
+BEGIN TRY
     DECLARE @Subcategories TABLE ([CategoryId] INT, [ParentId] INT, [Level] VARCHAR(MAX))
 	INSERT INTO @Subcategories
 	EXEC [dbo].[sp_select_all_subcategories_by_categoryid] @CategoryId
@@ -15,5 +17,19 @@ BEGIN
 		SELECT [categories].[CategoryId] 
 		FROM @Subcategories AS [categories]
 	)
+END TRY
+BEGIN CATCH
+	SELECT 
+        ERROR_NUMBER() AS ErrorNumber,
+        ERROR_SEVERITY() AS ErrorSeverity,
+        ERROR_STATE() AS ErrorState,
+        ERROR_PROCEDURE() AS ErrorProcedure,
+        ERROR_LINE() AS ErrorLine,
+        ERROR_MESSAGE() AS ErrorMessage;
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+END CATCH
+IF @@TRANCOUNT > 0
+    COMMIT TRANSACTION;
 END
 GO
